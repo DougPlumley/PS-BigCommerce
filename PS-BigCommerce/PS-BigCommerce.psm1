1732000724b8d2f8f3490bd56108dc3e703579c4
Douglas Plumley <douglas.plumley@dartmouth.edu>
2018-04-03 12:57:55 -0400

    Added Update-BCProduct, current parameters are somewhat limited and focused on the data I need to update for personal use.  I'd like to start looking at the Swagger specification as well and see if there is a better way to do this.

--- PS-BigCommerce/PS-BigCommerce.psm1
+++ PS-BigCommerce/PS-BigCommerce.psm1
@@ -54,3 +54,80 @@ function Get-BCProduct
     {
     }
 }
+
+function Update-BCProduct
+{
+    <#
+    .Synopsis
+    Short description
+    .DESCRIPTION
+    Long description
+    .EXAMPLE
+    Example of how to use this cmdlet
+    .EXAMPLE
+    Another example of how to use this cmdlet
+    #>
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Product SKU
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, Position=0)]
+        [string
+        $ID,
+
+        # Store URL including your unique store hash, I.E.: https://api.bigcommerce.com/stores/{store_hash}/v3/
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=1)]
+        [System.Uri]
+        $APIPath,
+
+        # Client-ID as username, Access Token as password.
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=2)]
+        [PSCredential]
+        $Credential,
+
+        [string]
+        $Name,
+
+        [string]
+        $SKU,
+
+        [float]
+        $Weight,
+
+        [float]
+        $Width,
+
+        [float]
+        $Height,
+
+        [float]
+        $Depth
+    )
+
+    Begin
+    {
+        $URI = "$($APIPath)catalog/products/$($ID)"
+    }
+    Process
+    {
+        $body = @{}
+
+        if ($Name) {$body.Add(Name, $Name)}
+        if ($SKU) {$body.Add(SKU, $SKU)}
+        if ($Weight) {$body.Add(Weight, $Weight)}
+        if ($Width) {$body.Add(Width, $Width)}
+        if ($Height) {$body.Add(Height, $Height)}
+        if ($Depth) {$body.Add(Depth, $Depth)}
+
+        Invoke-RestMethod -Uri $URI -ContentType "application/json" -Method Put -Headers @{
+            "Accept" = "application/json"
+            "X-Auth-Client" = $Credential.Username
+            "X-Auth-Token" = $Credential.GetNetworkCredential().Password
+        }
+    }
+    End
+    {
+    }
+}
